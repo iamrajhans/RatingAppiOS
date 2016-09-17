08de1a21b241d0519eb5966300941ff6e41d3507
iamrajhans <rajhans.jadhao@gmail.com>
2016-09-18 01:48:53 +0530

    added button select image from photolibrary and se to the imageView

--- RatingApp/RatingApp/MealViewController.swift
+++ RatingApp/RatingApp/MealViewController.swift
@@ -8,10 +8,40 @@
 
 import UIKit
 
-class MealViewController: UIViewController , UITextFieldDelegate{
+class MealViewController: UIViewController , UITextFieldDelegate, UIImagePickerControllerDelegate,UINavigationControllerDelegate,UIGestureRecognizerDelegate{
     
-
+    @IBOutlet weak var mealNameTextField: UITextField!
+    @IBOutlet weak var mealImageView: UIImageView!
     
-
+    override func viewDidLoad() {
+        
+    }
+    
+    
+    // Actions
+    @IBAction func selectImageFromPhotolibrary(sender: UITapGestureRecognizer) {
+        let imagePicker = UIImagePickerController ()
+        imagePicker.sourceType = .PhotoLibrary
+        
+        imagePicker.delegate = self
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        mealImageView.image = selectedImage
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    @IBAction func selectPicFromphotolib(sender: UIButton) {
+        let imagePicker = UIImagePickerController ()
+        imagePicker.sourceType = .PhotoLibrary
+        
+        imagePicker.delegate = self
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
 }
 
