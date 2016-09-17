53cc2eaced757382639105fde1b87f697d5c3676
iamrajhans <rajhans.jadhao@gmail.com>
2016-09-18 00:51:53 +0530

    renamed viewController file and added ui to creae new meal

---
+++ RatingApp/RatingApp/MealViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RatingApp
+//
+//  Created by Rajhans Jadhao on 11/09/16.
+//  Copyright © 2016 Rajhans Jadhao. All rights reserved.
+//
+
+import UIKit
+
+class MealViewController: UIViewController , UITextFieldDelegate{
+    
+
+    
+
+}
+
