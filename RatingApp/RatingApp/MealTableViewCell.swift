2b3d8fd0f223b7f63debdadb78b49bdb2953300b
iamrajhans <rajhans.jadhao@gmail.com>
2016-09-23 23:25:30 +0530

    added UIView in TableCell

--- RatingApp/RatingApp/MealTableViewCell.swift
+++ RatingApp/RatingApp/MealTableViewCell.swift
@@ -14,11 +14,27 @@ class MealTableViewCell: UITableViewCell {
     
     @IBOutlet weak var mealImage: UIImageView!
     
+    @IBOutlet weak var BackgroundCardView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         self.mealImage.layer.cornerRadius = self.mealImage.frame.size.width / 2
         self.mealImage.clipsToBounds = true
+        
+//        BackgroundCardView.layer.cornerRadius = 3.0
+//        BackgroundCardView.backgroundColor = UIColor.whiteColor()
+//        contentView.backgroundColor = UIColor(red: 240/255.0, green: 240/255.0, blue: 240/255.0, alpha: 1.0)
+//        BackgroundCardView.layer.masksToBounds = false
+//        BackgroundCardView.layer.shadowColor = UIColor.blackColor().colorWithAlphaComponent(0.2).CGColor
+//        BackgroundCardView.layer.shadowOffset = CGSize(width: 0, height: 0)
+//        
+//        BackgroundCardView.layer.shadowOpacity = 0.8
+
+        BackgroundCardView.layer.cornerRadius = 2
+        BackgroundCardView.layer.masksToBounds = false
+        BackgroundCardView.layer.shadowOpacity = 0.5
+        BackgroundCardView.layer.borderColor = UIColor.whiteColor().CGColor
+        BackgroundCardView.layer.borderWidth = 1.0
         
     }
 
